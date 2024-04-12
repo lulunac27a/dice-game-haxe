ab1bd221e866e150015c2b7793464de6b82614a5
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-04-11 21:09:59 -0500

    Add comments

--- dice-game/src/Main.hx
+++ dice-game/src/Main.hx
@@ -1,16 +1,16 @@
 class Main {
 	static function main() {
 		trace("Dice Game");
-		trace("Number of dice: ");
-		rollDice(Sys.stdin());
+		trace("Enter the number of dice to roll: ");
+		rollDice(Sys.stdin()); // get input to roll the dice based on input
 	}
 
 	static function rollDice(input:haxe.io.Input) {
-		var numberOfDice:Int = Std.parseInt(input.readLine());
-		var dice:Array<Int> = [];
+		var numberOfDice:Int = Std.parseInt(input.readLine()); // parse input to integer
+		var dice:Array<Int> = []; // initialize empty array of dice
 		for (i in 0...numberOfDice) {
-			dice[i] = Std.random(6);
-			trace('Dice ${i + 1}: ${dice[i]}');
+			dice[i] = Std.random(6); // add dice array to rolled die
+			trace('Dice ${i + 1}: ${dice[i]}'); // print die roll value
 		}
 	}
 }
