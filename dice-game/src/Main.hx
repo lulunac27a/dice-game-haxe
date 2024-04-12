cd0874bbe7f91cdf516e969b3332df81fe715b58
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-04-11 21:13:52 -0500

    Add dice sum

--- dice-game/src/Main.hx
+++ dice-game/src/Main.hx
@@ -5,12 +5,15 @@ class Main {
 		rollDice(Sys.stdin()); // get input to roll the dice based on input
 	}
 
-	static function rollDice(input:haxe.io.Input) {
+	static function rollDice(input:haxe.io.Input):Void {
 		var numberOfDice:Int = Std.parseInt(input.readLine()); // parse input to integer
 		var dice:Array<Int> = []; // initialize empty array of dice
-		for (i in 0...numberOfDice) {
+		var sum:Int = 0; // initialize sum to 0
+		for (i in 0...numberOfDice) { // loop to entered number of dice based on input
 			dice[i] = Std.random(6); // add dice array to rolled die
+			sum += dice[i]; // add die value to sum
 			trace('Dice ${i + 1}: ${dice[i]}'); // print die roll value
 		}
+		trace('Sum of dice: ${sum}'); // print sum of all dice
 	}
 }
