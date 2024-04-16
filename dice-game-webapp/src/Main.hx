ba7f8a6012acf0059cc59f4dc8c3fcc5e5e28269
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-04-15 20:07:02 -0500

    Add dice sum

--- dice-game-webapp/src/Main.hx
+++ dice-game-webapp/src/Main.hx
@@ -9,13 +9,16 @@ class Main {
 			var numberOfDice:Int = Std.parseInt(diceInput.value); // get number of dice to roll
 			appContent.innerHTML = ""; // set app content to empty string
 			var dice:Array<Int> = []; // initialize dice array
+			var sum:Int = 0; // initialize sum to 0
 			for (i in 0...numberOfDice) {
 				dice[i] = Std.random(6) + 1; // roll the dice
+				sum += dice[i]; // add die value to sum
 				var newDice:js.html.DivElement = cast(js.Browser.document.createElement("div"), js.html.DivElement);
 				newDice.id = 'dice-${i + 1}'; // set id to dice-number to make id's unique
 				newDice.textContent = 'Dice ${i + 1}: ${dice[i]}'; // set text to each die value
 				appContent.appendChild(newDice); // append dice values to app content
 			}
+			appContent.innerHTML += '\nSum of dice: ${sum}'; // print sum of all dice and append to app content
 		}
 	}
 }
