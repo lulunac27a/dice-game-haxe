37959862d736c229152790f50bf45cbd27ba4ae3
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-04-15 19:50:39 -0500

    Add type hints

--- dice-game-webapp/src/Main.hx
+++ dice-game-webapp/src/Main.hx
@@ -2,16 +2,16 @@ import js.Browser;
 
 class Main {
 	static function main() {
-		var appContent = cast(Browser.document.getElementById("app"), js.html.DivElement);
-		var rollButton = cast(Browser.document.getElementById("roll"), js.html.ButtonElement);
-		var diceInput = cast(Browser.document.getElementById("dice"), js.html.InputElement);
+		var appContent:js.html.DivElement = cast(Browser.document.getElementById("app"), js.html.DivElement);
+		var rollButton:js.html.ButtonElement = cast(Browser.document.getElementById("roll"), js.html.ButtonElement);
+		var diceInput:js.html.InputElement = cast(Browser.document.getElementById("dice"), js.html.InputElement);
 		rollButton.onclick = (event) -> {
-			var numberOfDice = Std.parseInt(diceInput.value);
+			var numberOfDice:Int = Std.parseInt(diceInput.value);
 			appContent.innerHTML = "";
-			var dice = [];
+			var dice:Array<Int> = [];
 			for (i in 0...numberOfDice) {
 				dice[i] = Std.random(6) + 1;
-				var newDice = js.Browser.document.createElement("div");
+				var newDice:js.html.DivElement = cast(js.Browser.document.createElement("div"), js.html.DivElement);
 				newDice.id = 'dice-${i + 1}';
 				newDice.textContent = 'Dice ${i + 1}: ${dice[i]}';
 				appContent.appendChild(newDice);
